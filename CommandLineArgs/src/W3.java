3310989c72746ad84b15e7031c885ea62476abc6
Indu <Indu@Dell>
2020-07-03 12:47:46 +0530

    Command line arguments

---
+++ CommandLineArgs/src/W3.java
@@ -0,0 +1,10 @@
+
+public class W3 {
+	public static void main(String args[]) {
+	      int a,b;
+	      a=Integer.parseInt(args[0]);
+	      b=Integer.parseInt(args[1]);
+	      System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
+	    }
+
+}
